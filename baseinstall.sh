f2004aa9a920e66e094c59c57c26f00a5682baeb
Tri-Tech <craig.coleman@ksd.org>
2018-01-19 12:03:27 -0800

    Update baseinstall.sh

--- baseinstall.sh
+++ baseinstall.sh
@@ -1,5 +1,7 @@
 apt-get update -y;
 apt-get upgrade  -y;
+apt-get install curl;
+apt-get install git;
 apt-get install geany geany-plugins sudo chromium -y;
 systemctl set-default multi-user.target;
 adduser dock sudo;
